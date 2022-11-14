9af99cbe86c86823480bfcdd3fd67af99e73da6e
Alec Winograd <alec@ouitherapeutics.com>
2022-11-14 17:44:51 -0600

    Global decorator repro

--- .ondevice/preview.js
+++ .ondevice/preview.js
@@ -1,6 +1,23 @@
 import { withBackgrounds } from "@storybook/addon-ondevice-backgrounds";
 
-export const decorators = [withBackgrounds];
+import React from "react";
+import { View } from "react-native";
+import { DecoratorFunction } from "@storybook/client-api";
+
+// Add global decorator that wraps the story with a visible padding. When updating Button story's
+// text to something like "Hello world 1" you'll see the red background double
+export const Container: DecoratorFunction<React.ReactElement> = (
+  story,
+  { parameters }
+) => {
+  return (
+    <View style={{ backgroundColor: "red", padding: 5 }}>
+      {React.createElement(story)}
+    </View>
+  );
+};
+
+export const decorators = [Container];
 
 export const parameters = {
   backgrounds: [
